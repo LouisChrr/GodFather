f52e009981ca7819854a4af4e927ce4baa5de503
Elise Nguyen <elise.chuyen.nguyen@gmail.com>
2020-09-02 10:47:04 +0200

    Ajout des abstract dans LevelBin
    Ajout abstract LevelVin

--- Assets/Scripts/LevelBin.cs
+++ Assets/Scripts/LevelBin.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/*
+ * Contient référence au Level de Bin
+ * Contient une méthode déclenchée au démarrage du level
+ * Contient une update du Level
+ */
 public abstract class LevelBin
 {
     public LevelStateBin stateBin;
@@ -10,6 +16,11 @@ public abstract class LevelBin
     {
         stateBin = bin;
     }
+
+    public abstract void StateBegin();
+    public abstract void StateEnd();
+    public abstract void StateUpdate();
+    public abstract void StateUpdateState();
 
 
 }
