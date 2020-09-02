73e8f11ff5324118054cfb548a6f921ad8ed1854
Elise Nguyen <elise.chuyen.nguyen@gmail.com>
2020-09-02 14:27:09 +0200

    maj PoubelleCOntroller

--- Assets/Scripts/PoubelleController.cs
+++ Assets/Scripts/PoubelleController.cs
@@ -7,11 +7,8 @@ public class PoubelleController : MonoBehaviour
 {
     public Vector2 movement;
     public float moveSpeed = 10;
-<<<<<<< HEAD
     //public Bin poubelle;
-=======
     public int score;
->>>>>>> 76cdffb95e8ba0aea5ef16ceef77efe1d7828e39
 
     // Start is called before the first frame update
     void Start()
@@ -22,13 +19,8 @@ public class PoubelleController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
         //transform.Translate(Vector2.left*Time.deltaTime);
         transform.Translate(movement * Time.deltaTime);
-=======
-        
-        
->>>>>>> 76cdffb95e8ba0aea5ef16ceef77efe1d7828e39
     }
 
     void OnMove(InputValue value)
