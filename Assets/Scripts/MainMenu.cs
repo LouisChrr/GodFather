e4f9edece81418304fad7575df3c5d98c5011e3b
Vincent <vincent_1999@hotmail.fr>
2020-09-02 10:38:32 +0200

    Add main menu and player selection

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+   [SerializeField] private GameObject allButtonsPanel;
+   [SerializeField] private GameObject loadingPanel;
+   [SerializeField] private GameObject optionPanel;
+
+    public void PlayButton(string nameMainScene)
+    {
+        allButtonsPanel.SetActive(false);
+        loadingPanel.SetActive(true);
+        SceneManager.LoadScene(nameMainScene);
+    }
+
+    public void OptionButtons()
+    {
+        allButtonsPanel.SetActive(false);
+        optionPanel.SetActive(true);
+    }
+
+    public void ReturnButtons()
+    {
+        allButtonsPanel.SetActive(true);
+        optionPanel.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/PlayerSelection.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelection : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] players;
+
+    int playerSelected = 0;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            SelectPreviousPlayer();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            SelectNextPlayer();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print(GetPlayerSelected());
+        }
+    }
+
+    public void SelectNextPlayer()
+    {
+        players[playerSelected].transform.GetChild(0).gameObject.SetActive(false);
+        if (playerSelected + 1 == players.Length)
+        {
+            playerSelected = 0;
+        }
+        else
+        {
+            playerSelected++;
+        }
+        players[playerSelected].transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    public void SelectPreviousPlayer()
+    {
+        players[playerSelected].transform.GetChild(0).gameObject.SetActive(false);
+        if (playerSelected == 0)
+        {
+            playerSelected = players.Length - 1;
+        }
+        else
+        {
+            playerSelected--;
+        }
+        players[playerSelected].transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    public int GetPlayerSelected()
+    {
+        return playerSelected + 1;
+    }
+
+}
