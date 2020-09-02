adfb6ce36bf3fd8cdb4eed06caad97a4abd710f6
LouisChrr <louis.charrieau@edu.devinci.fr>
2020-09-02 16:41:26 +0200

    Merge scene mercredi

---
+++ Assets/Scripts/PoubelleIdentifier.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PoubelleIdentifier : MonoBehaviour
+{
+    public int ID;
+    private Color PoubelleColor;
+    bool ready = false;
+    bool canBeReady = false;
+    // Start is called before the first frame update
+    void Start()
+      {
+        PreGameManager.Instance.poubelles.Add(this);
+        ID = PreGameManager.Instance.playersNumber;
+        PoubelleColor = PreGameManager.Instance.colors[ID-1];
+
+        Invoke("CanBeReady", 0.5f);
+
+
+      }
+
+    void CanBeReady(){
+        canBeReady = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDeviceLost(){
+
+    }
+
+    void OnDeviceRegained(){
+
+
+    }
+
+    void OnGrab()
+    {
+    if(PreGameManager.Instance.hasGameStarted == true || ready == true || canBeReady == false) return;
+        PreGameManager.Instance.OnPlayerReady();
+        ready = true;
+
+        PreGameManager.Instance.poubellesUI[ID-1].readyText.SetActive(true);
+        print("Grab!");
+    }
+
+}
--- Assets/Scripts/PoubelleIdentifierUI.cs
+++ Assets/Scripts/PoubelleIdentifierUI.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
-public class PoubelleIdentifier : MonoBehaviour
+public class PoubelleIdentifierUI : MonoBehaviour
 {
     public int ID;
     private Color PoubelleColor;
+    public GameObject readyText;
 
     // Start is called before the first frame update
     void Start()
     {
-        PreGameManager.Instance.poubelles.Add(this);
+        PreGameManager.Instance.poubellesUI.Add(this);
         ID = PreGameManager.Instance.playersNumber;
         PoubelleColor = PreGameManager.Instance.colors[ID-1];
+
+        this.GetComponent<Image>().color = PoubelleColor;
     }
 
     // Update is called once per frame
