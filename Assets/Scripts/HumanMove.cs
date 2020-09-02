5e49a4cf057ab265c474c343a23775ef7688a061
Elise Nguyen <elise.chuyen.nguyen@gmail.com>
2020-09-02 17:23:06 +0200

    Création du spawn

--- Assets/Scripts/HumanMove.cs
+++ Assets/Scripts/HumanMove.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Human : MonoBehaviour
+public class HumanMove : MonoBehaviour
 {
     public Vector2 position;
+    public int orient;
+    public float humanSpeed = 1;
 
     // Start is called before the first frame update
     void Start()
