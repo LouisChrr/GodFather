850a4958ade83ed10f5dfe3423d3098eb6276a0c
Elise Nguyen <elise.chuyen.nguyen@gmail.com>
2020-09-02 15:38:07 +0200

    maj Poubelle controller

--- Assets/Scripts/PoubelleController.cs
+++ Assets/Scripts/PoubelleController.cs
@@ -6,21 +6,26 @@ using UnityEngine.InputSystem;
 public class PoubelleController : MonoBehaviour
 {
     public Vector2 movement;
-    public float moveSpeed = 10;
+    public Vector2 position;
+    public int moveSpeed = 1;
     //public Bin poubelle;
     public int score;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //position = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         //transform.Translate(Vector2.left*Time.deltaTime);
-        transform.Translate(movement * Time.deltaTime);
+        position.x += movement.x * Time.deltaTime * moveSpeed;
+        position.y += movement.y * Time.deltaTime * moveSpeed;
+        transform.position = new Vector2(position.x, position.y);
+        //transform.Translate(movement * Time.deltaTime);
+        //transform.Tr
     }
 
     void OnMove(InputValue value)
