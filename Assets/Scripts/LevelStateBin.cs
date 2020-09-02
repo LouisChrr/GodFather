fca1d649d300dfbc49830ceb79503c647bef6767
Elise Nguyen <elise.chuyen.nguyen@gmail.com>
2020-09-02 10:27:59 +0200

    Ajout script LevelBin et LevelStateBin
    script LevelStateBin et LevelBin

--- Assets/Scripts/LevelStateBin.cs
+++ Assets/Scripts/LevelStateBin.cs
@@ -2,17 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class
 {
-    public Vector2 position;
-    public LevelBin level = null;
-    public GameObject sprite;
-    
-
     // Start is called before the first frame update
     void Start()
     {
-        level = new LevelBin(this);
+        
     }
 
     // Update is called once per frame
