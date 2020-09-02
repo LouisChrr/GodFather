014b83fd6bdf6e6eced6395c856ab78bf987ef7a
Elise Nguyen <elise.chuyen.nguyen@gmail.com>
2020-09-02 17:13:19 +0200

    Création script Human

---
+++ Assets/Scripts/Human.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour
+{
+    public Vector2 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void Move()
+    {
+
+    }
+}
