3c16ba4c3fd61c90cbfec7a67061e9fe956ab1d4
Vincent <vincent_1999@hotmail.fr>
2020-09-02 18:48:12 +0200

    Add UI in game

---
+++ Assets/Scripts/ScorePlayerUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScorePlayerUI : MonoBehaviour
+{
+
+    [SerializeField] GameObject[] playerScoreUI;
+    [SerializeField] RectTransform[] playerScoreLevelProgressionUI;
+    [SerializeField] Text[] playerTextScoreUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DisplayUiForXPlayers(4);
+        UpdateScoreOfPlayer(1,50,5);
+    }
+
+    public void DisplayUiForXPlayers(int nbPlayers)
+    {
+        while (nbPlayers > 0)
+        {
+            nbPlayers--;
+            playerScoreUI[nbPlayers].SetActive(true);
+        }
+    }
+
+    public void UpdateScoreOfPlayer(int idPlayer, int pourcentageOfLevelBar, int score)
+    {
+        playerTextScoreUI[idPlayer - 1].text = score.ToString();
+        playerScoreLevelProgressionUI[idPlayer - 1].sizeDelta = new Vector2 ( ((float)pourcentageOfLevelBar / 100) * 615, playerScoreLevelProgressionUI[idPlayer - 1].sizeDelta.y);
+    }
+
+}
