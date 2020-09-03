25b3cefbd1a976092115affa0ddb1198dadeaf3c
LouisChrr <louis.charrieau@edu.devinci.fr>
2020-09-03 10:11:52 +0200

    Anims

--- Assets/Scripts/PoubelleController.cs
+++ Assets/Scripts/PoubelleController.cs
@@ -33,51 +33,128 @@ public class PoubelleController : MonoBehaviour
         ModifScore();
         ChangeLevel();
         //print(level);
-        //transform.Translate(movement * Time.deltaTime);
-        //transform.Tr
-        
+
     }
 
     private void FixedUpdate() {
-        ResetAnimBools();
+        //ResetAnimBools();
 
-        if(movement.x > 0.1f ){// SI A DROITE
-
+        if(movement.x > 0.15f ){// SI A DROITE
             if(movement.y > 0.1f){//SI EN HAUT
-                if(movement.y < 0.3f){//SI DIAGO
-                      anim.SetBool("TopRight", true);
+                if(movement.y < 0.4f){//SI DIAGO
+
+                anim.SetBool("Top", false);
+                anim.SetBool("Front", false);
+                anim.SetBool("FrontLeft", false);
+                anim.SetBool("TopLeft", false);
+                anim.SetBool("Right", false);
+                anim.SetBool("FrontRight", false);
+                anim.SetBool("Left", false);
+
+                anim.SetBool("TopRight", true);
                 }
-            }else if(movement.y < 0.1f){//SI EN BAS
-                if(movement.y > 0.3f){//SI DIAGO
-                      anim.SetBool("FrontRight", true);
+            }else if(movement.y < -0.1f){//SI EN BAS
+                if(movement.y > -0.4f){//SI DIAGO
+
+                anim.SetBool("Top", false);
+                anim.SetBool("Front", false);
+                anim.SetBool("FrontLeft", false);
+                anim.SetBool("TopLeft", false);
+                anim.SetBool("Right", false);
+                anim.SetBool("TopRight", false);
+                anim.SetBool("Left", false);
+
+                anim.SetBool("FrontRight", true);   
                 }
             }else{// SI FULL DROITE
+
+                anim.SetBool("Top", false);
+                anim.SetBool("Front", false);
+                anim.SetBool("FrontLeft", false);
+                anim.SetBool("TopLeft", false);
+                anim.SetBool("FrontRight", false);
+                anim.SetBool("TopRight", false);
+                anim.SetBool("Left", false);
+
                 anim.SetBool("Right", true);
             }
 
             
 
-        }else if(movement.x < 0.1f){//SI A GAUCHE
+        }else if(movement.x < 0.15f){//SI A GAUCHE
 
             if(movement.y > 0.1f){//SI EN HAUT
-                if(movement.y < 0.3f){//SI DIAGO
-                      anim.SetBool("TopLeft", true);
+                if(movement.y < 0.4f){//SI DIAGO
+
+                anim.SetBool("Top", false);
+                anim.SetBool("Front", false);
+                anim.SetBool("FrontLeft", false);
+                anim.SetBool("Right", false);
+                anim.SetBool("FrontRight", false);
+                anim.SetBool("TopRight", false);
+                anim.SetBool("Left", false);
+
+                anim.SetBool("TopLeft", true);
                 }
-            }else if(movement.y < 0.1f){//SI EN BAS
-                if(movement.y > 0.3f){//SI DIAGO
-                      anim.SetBool("FrontLeft", true);
+            }else if(movement.y < -0.1f){//SI EN BAS
+                if(movement.y > -0.4f){//SI DIAGO
+                        
+                anim.SetBool("Top", false);
+                anim.SetBool("Front", false);
+                anim.SetBool("TopLeft", false);
+                anim.SetBool("Right", false);
+                anim.SetBool("FrontRight", false);
+                anim.SetBool("TopRight", false);
+                anim.SetBool("Left", false);
+
+                anim.SetBool("FrontLeft", true);
                 }
             }else{// SI FULL GAUCHE
+                anim.SetBool("Top", false);
+                anim.SetBool("Front", false);
+                anim.SetBool("FrontLeft", false);
+                anim.SetBool("TopLeft", false);
+                anim.SetBool("Right", false);
+                anim.SetBool("FrontRight", false);
+                anim.SetBool("TopRight", false);
+
+
+
                 anim.SetBool("Left", true);
             }
 
 
-
         }else{//SI TOP OU BAS
                 if(movement.y >0){//SI EN HAUT
-            anim.SetBool("Top", true);
+           
+
+                anim.SetBool("Front", false);
+                anim.SetBool("FrontLeft", false);
+                anim.SetBool("TopLeft", false);
+                anim.SetBool("Right", false);
+                anim.SetBool("FrontRight", false);
+                anim.SetBool("TopRight", false);
+                anim.SetBool("Left", false);
+
+                anim.SetBool("Top", true);
+
                 }else{//SI EN BAS
-            anim.SetBool("Front", true);
+                anim.SetBool("Top", false);
+                anim.SetBool("FrontLeft", false);
+                anim.SetBool("TopLeft", false);
+                anim.SetBool("Right", false);
+                anim.SetBool("FrontRight", false);
+                anim.SetBool("TopRight", false);
+                anim.SetBool("Left", false);
+
+
+                anim.SetBool("Front", true);
+
+                
+
+
+                
+
                 }
         }
     }
