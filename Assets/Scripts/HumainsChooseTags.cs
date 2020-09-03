88e0fdf4491ea96bff037322732a36d61c3162c6
Vincent <vincent_1999@hotmail.fr>
2020-09-03 14:53:53 +0200

    Different human

---
+++ Assets/Scripts/HumainsChooseTags.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumainsChooseTags : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int randTag = Random.Range(0,4);
+        switch (randTag)
+        {
+            case 0 :
+                gameObject.tag = "Blue";
+                break;
+            case 1:
+                gameObject.tag = "Brown";
+                break;
+            case 2:
+                gameObject.tag = "Yellow";
+                break;
+            case 3:
+                gameObject.tag = "Green";
+                break;
+            case 4:
+                gameObject.tag = "None";
+                break;
+        }
+
+    }
+
+}
