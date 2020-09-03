6aa2c47188e3479c2fd83fbc4060b530ba004871
Vincent <vincent_1999@hotmail.fr>
2020-09-03 15:17:50 +0200

    Update horloge

---
+++ Assets/Scripts/Horloge.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Horloge : MonoBehaviour
+{
+
+    [SerializeField]Text textHorloge;
+    int minute = 1;
+    int seconde = 30;
+
+    float timer = 1f;
+
+    void Start()
+    {
+        textHorloge.text = minute.ToString() + seconde.ToString(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0f)
+        {
+            timer = 1f;
+            if (seconde == 0)
+            {
+                if (minute > 0)
+                {
+                    seconde = 59;
+                    minute--;
+                }
+                else
+                {
+                    print("END");
+                }
+            }
+            else
+            {
+                seconde--;
+            }
+
+            textHorloge.text = minute.ToString() + " : " + seconde.ToString();
+        }
+
+    }
+}
